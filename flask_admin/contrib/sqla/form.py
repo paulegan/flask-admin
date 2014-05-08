47f2ef598c23d2b08d57e08e55f9c77975f3d68e
Paul Egan <paulegan@mail.com>
2014-05-08 23:23:29 +0100

    test for issue with inline_models and "self" relationship

--- flask_admin/contrib/sqla/form.py
+++ flask_admin/contrib/sqla/form.py
@@ -550,7 +550,7 @@ class InlineModelConverter(InlineModelConverterBase):
         reverse_prop = None
 
         for prop in target_mapper.iterate_properties:
-            if hasattr(prop, 'direction'):
+            if hasattr(prop, 'direction') and prop.direction.name == 'MANYTOONE':
                 if issubclass(model, prop.mapper.class_):
                     reverse_prop = prop
                     break
@@ -561,7 +561,7 @@ class InlineModelConverter(InlineModelConverterBase):
         forward_prop = None
 
         for prop in mapper.iterate_properties:
-            if hasattr(prop, 'direction'):
+            if hasattr(prop, 'direction') and prop.direction.name == 'ONETOMANY':
                 if prop.mapper.class_ == target_mapper.class_:
                     forward_prop = prop
                     break
